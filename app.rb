0ac7be1d6c3e8ae25398c77d18640270cc6da6c2
Jamal Springer <jamal.springer@outlook.comm>
2019-08-05 18:20:39 +0100

    replaced the random name generation with the params method to accept a name param in the url request

--- app.rb
+++ app.rb
@@ -15,10 +15,14 @@ end
 get '/monster' do
    "¯¯̿̿¯̿̿'̿̿̿̿̿̿̿'̿̿'̿̿̿̿̿'̿̿̿)͇̿̿)̿̿̿̿ '̿̿̿̿̿̿\̵͇̿̿\=(•̪̀●́)=o/̵͇̿̿/'̿̿ ̿ ̿̿"
 end
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
 get '/leopard' do
   erb(:leopard)
+end
+get '/named-cat' do
+  @name = params[:name]
+  erb(:index)
 end
