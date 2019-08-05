4efa2f493a85223a7dc4632e5ae3166fe13b9292
Jamal Springer <jamal.springer@outlook.comm>
2019-08-05 17:34:30 +0100

    Added leopard.erb file and moved the html/css for the leopard page there

--- app.rb
+++ app.rb
@@ -19,9 +19,5 @@ get '/cat' do
   erb(:index)
 end
 get '/leopard' do
-  "<div class='warning'>
-     <img src='https://conservationaction.co.za/wp-content/uploads/2017/01/leopard.jpg'>
-          alt='An intimidating leopard.'>
-     <p>Beware of the leopard</p>
-   </div>"
+  erb(:leopard)
 end
