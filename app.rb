44a691be3c57ff1d4828b657751e78123a864ac7
Jamal Springer <jamal.springer@outlook.comm>
2019-08-07 16:36:24 +0100

    added a new erb file containing a form

--- app.rb
+++ app.rb
@@ -26,3 +26,11 @@ get '/named-cat' do
   @name = params[:name]
   erb(:index)
 end
+get '/cat-form' do
+  erb :cat_form
+end
+post '/named-cat' do
+   p params
+   @name = params[:name]
+   erb :index
+end
